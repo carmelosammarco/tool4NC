1a7221cbb6868c0aa5dbefe1b359dbe9a014c5b3
Carmelo <carmelosammarco@users.noreply.github.com>
2019-02-24 13:34:30 +0100

    Upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='tool4nc1',
+      version='0.1',
+      description='Software for the netCDF files manipulations',
+      long_description=long_description,
+      url="http://example.com/HelloWorld/",
+      author='Carmelo Sammarco',
+      author_email='sammarcocarmelo@gmail.com',
+      license='MIT',
+      install_requires=[
+        'numpy>=1.15.4', 
+        'pandas>=0.23.4', 
+        'xarray>=0.11.0', 
+        'csv342>=1.0.0', 
+        'shapely>=1.6.4.post2', 
+        'fiona>=1.8.4', 
+        'cdo>=1.4.0' ],
+      packages=find_packages(),
+      
+)
