4f2a2653111d8706b01204c9a3fbc6f4223eb12d
Carmelo <carmelosammarco@users.noreply.github.com>
2019-03-03 04:06:31 +0100

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='tool4nc',
-      version='0.1.5',
+      version='0.1.6',
       description='Software for the netCDF files manipulations',
       long_description=long_description,
       long_description_content_type='text/markdown',
