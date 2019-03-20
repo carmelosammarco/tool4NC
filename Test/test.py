3ea618eb94120967dd946230cdbe831afce23a1e
Carmelo <carmelosammarco@users.noreply.github.com>
2019-03-20 18:52:38 +0100

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -2,19 +2,19 @@ from tool4nc import *
 import os
 import glob
 
-test_folder = os.path.join("Test")
-Output_folder = os.path.join("Test", "OUT")
+test_folder = "Test/"
+Output_folder = "Test/OUT"
 
-file_to_use = test_folder + "/VHM0.nc"
+file_to_use = "Test/VHM0.nc"
 Variable = "VHM0"
 
-#nctocsv(file_to_use, Output_folder)
-#nctoshape(file_to_use, Output_folder, Variable)
-#nctogdr(file_to_use, Output_folder)
-#splitnc(file_to_use, Output_folder, "DAY", "Test")
+nctocsv(file_to_use, Output_folder)
+nctoshape(file_to_use, Output_folder, Variable)
+nctogdr(file_to_use, Output_folder)
+splitnc(file_to_use, Output_folder, "DAY", "Test")
 
-files = glob.glob(Output_folder + "/*")
-for f in files:
+results = glob.glob("Test/OUT/*")
+for f in results:
     os.remove(f)
 
 
