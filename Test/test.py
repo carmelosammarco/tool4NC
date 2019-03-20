ddcff4c6203c27766798fea4a0875ee0557a1a0c
Carmelo <carmelosammarco@users.noreply.github.com>
2019-03-20 16:07:00 +0100

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -8,10 +8,10 @@ Output_folder = os.path.join("Test", "OUT")
 file_to_use = test_folder + "/VHM0.nc"
 Variable = "VHM0"
 
-nctocsv(file_to_use, Output_folder)
-nctoshape(file_to_use, Output_folder, Variable)
-nctogdr(file_to_use, Output_folder)
-splitnc(file_to_use, Output_folder, "DAY", "Test")
+#nctocsv(file_to_use, Output_folder)
+#nctoshape(file_to_use, Output_folder, Variable)
+#nctogdr(file_to_use, Output_folder)
+#splitnc(file_to_use, Output_folder, "DAY", "Test")
 
 files = glob.glob(Output_folder + "/*")
 for f in files:
