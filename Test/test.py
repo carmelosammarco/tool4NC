1f4082c84814459e0abeef9af162cbbedd2aea06
Carmelo <carmelosammarco@users.noreply.github.com>
2019-03-20 15:56:55 +0100

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -1,13 +1,9 @@
-from tool4nc import *
-from pathlib import Path, PureWindowsPath 
+from tool4nc import * 
 import os
 import glob
 
-test_folder = Path("Test/")
-Output_folder = Path("Test/OUT")
-
-test_folder_pathwindow = PureWindowsPath(test_folder)
-OUT_pathwindow = PureWindowsPath(Output_folder)
+test_folder = os.path.join("Test")
+Output_folder = os.path.join("Test", "OUT")
 
 file_to_use = test_folder + "/VHM0.nc"
 Variable = "VHM0"
