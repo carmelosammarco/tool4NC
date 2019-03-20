194166e7ff5903f09b5cbea5069cfdcf0f0d4825
Carmelo <carmelosammarco@users.noreply.github.com>
2019-03-20 19:28:42 +0100

    General

--- Test/test.py
+++ Test/test.py
@@ -6,12 +6,12 @@ test_folder = "/home/travis/build/carmelosammarco/tool4nc/Test/"
 Output_folder = "/home/travis/build/carmelosammarco/tool4nc/Test/OUT"
 
 file_to_use = "/home/travis/build/carmelosammarco/tool4nc/Test/VHM0.nc"
-Variable = "VHM0"
+#Variable = "VHM0"
 
 nctocsv(file_to_use, Output_folder)
-nctoshape(file_to_use, Output_folder, Variable)
-nctogdr(file_to_use, Output_folder)
-splitnc(file_to_use, Output_folder, "DAY", "Test")
+#nctoshape(file_to_use, Output_folder, Variable)
+#nctogdr(file_to_use, Output_folder)
+#splitnc(file_to_use, Output_folder, "DAY", "Test")
 
 results = glob.glob("/home/travis/build/carmelosammarco/tool4nc/Test/OUT/*")
 for f in results:
